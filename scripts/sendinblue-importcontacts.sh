ce433e134b966941f5dd1de763e1965ea0863fab
Laurent Malvert <laurent.malvert.freelance@gmail.com>
2021-07-13 19:59:46 +0200

    rework contacts import script to be less memory hungry
    It seems we still hit the occasional segmentation fault on GitHub actions when
    running `jq`, so I assume it's due to memory consumption (although it's hard to
    tell and our files aren't big).
    
    Several to work around this issue, but for now trying to use a simpler jq filter
    that can use streaming mode (it didn't before, as I used slurp mode and not
    really streaming), and then re-processing that with sed and awk to inject into
    a payload template to send to sendinblue with curl.
    
    This should work fine... *if* GitHub actions support awk...

--- scripts/sendinblue-importcontacts.sh
+++ scripts/sendinblue-importcontacts.sh
@@ -7,39 +7,36 @@ SENDINBLUE_API_KEY="${3}"
 # there are simpler ways to do this, but we get a segmentation fault
 # in jq on GitHub actions, so we build this gradually.
 csv_file_to_json_payload() {
-    echo '
-        {
-            "listIds": [
-                '"${SENDINBLUE_LIST_ID}"'
-            ],
-            "emailBlacklist": false,
-            "smsBlacklist": false,
-            "updateExistingContacts": true,
-            "emptyContactsAttributes": false,
-            "fileBody": ' | tr -d '\n' | tr -d '\r' > payload.header.json
-    echo '}' > payload.footer.json
+    # transform csv to tsv
     cat "${CSV_FILE_PATH}" | sed 's/,/;/g' > data.csv
-    cat data.csv							\
-	| jq --stream -Rras '.'						\
-        | sed -e 's/^"//' -e 's/"$//' | sed '1s/^/"/' | sed '$s/$/"/' > payload.data.json
-    cat									\
-	payload.header.json						\
-	payload.data.json						\
-	payload.footer.json > payload.json
+
+    # transform to JSON data field
+    # ensure correct escaping for JSON for each TSV line (with jq)
+    # then remove trailing/leading " added by JQ on each line
+    # then regroup all lines with awk using literal newlines
+    jq --stream -R '.' data.csv         \
+	| sed -e 's/^"//' -e 's/"$//'   \
+	| awk '{printf "%s\\n", $0}' > payload.data.json
+
+    # inject data into payload template
+    cat payload.template.json | \
+	sed 's/__LIST_ID__/'"${SENDINBLUE_LIST_ID}"'/' | \
+	awk 'BEGIN{getline l < "payload.data.json"}/__DATA__/{gsub("__DATA__",l)}1' \
+	    > payload.json
 }
 
 if [ -n "${CSV_FILE_PATH}" ] && [ -n "${SENDINBLUE_LIST_ID}" ]; then
     if [ -f "${CSV_FILE_PATH}" ]; then
         csv_file_to_json_payload
-        curl								\
-	          -s							\
-		  -o output.json					\
-	          --request POST					\
-	          --url https://api.sendinblue.com/v3/contacts/import	\
-	          --header "Accept: application/json"			\
-	          --header "Content-Type: application/json"		\
-	          --header "api-key: ${SENDINBLUE_API_KEY}"		\
-	          --data @payload.json
+        curl                                                            \
+                  -s                                                    \
+                  -o output.json                                        \
+                  --request POST                                        \
+                  --url https://api.sendinblue.com/v3/contacts/import   \
+                  --header "Accept: application/json"                   \
+                  --header "Content-Type: application/json"             \
+                  --header "api-key: ${SENDINBLUE_API_KEY}"             \
+                  --data @payload.json
     else
         echo "Please provide a valid CSV input file."
         exit 24;
